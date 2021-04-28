1b1f8d3ad7579871c0060387698e279a96563833
Apurva Sawant <apurvasawant45@gmail.com>
2021-04-28 19:11:51 +0530

    complete input output functionality

---
+++ app.js
@@ -0,0 +1,31 @@
+var btnTranslate = document.querySelector("#btn-translate")
+var txtInput = document.querySelector("#txt-input")
+var output = document.querySelector("#output")
+
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+//var serverURL="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("error occured", error)
+    alert("Something went wrong!!. Please try again later")
+}
+
+
+function clickHandler() {
+
+    var inputText = txtInput.value
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            output.innerText = translatedText
+        })
+        .catch(errorHandler)
+
+}
+
+btnTranslate.addEventListener("click", clickHandler)
